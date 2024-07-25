0d5c0210081933c062555cf79a366d714553db2a
Reda Ali <reda5201028@gmail.com>
2024-07-25 14:36:13 +0300

    Middlewares

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 app
   .route("/api/v1/tours")
   .get((req, res) => {
