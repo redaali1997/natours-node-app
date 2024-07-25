47fba660695602391d5e223b3f033bbe63b3b9c3
Reda Ali <reda5201028@gmail.com>
2024-07-25 14:36:00 +0300

    Routes

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const morgan = require("morgan");
 
 const app = express();
 
+app
+  .route("/api/v1/tours")
+  .get((req, res) => {
+    res.status(200).json({
+      status: "success",
+      requestedAt: req.requestTime,
+      tours: "tours",
+    });
+  })
+  .post((req, res) => {
+    res.status(200).json({
+      status: "success",
+      tours: req.body,
+    });
+  });
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    tours: `one tour ${req.params.id}`,
+  });
+});
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log("App running on port " + port);
